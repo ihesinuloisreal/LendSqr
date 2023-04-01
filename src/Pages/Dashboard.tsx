604b2a4b7778ae0a5e94a73ec40495fd08c8a729
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-01 15:53:32 -0700

    Dashbord nav

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -7,24 +7,28 @@ import arrow_d from "../images/arrow_d.png"
 export function Dashboard() {
     return (
         <>
-            <nav className="top_nav">
-                <div className="">
-                            
-                    <h1 className=""><img className="" src={Union} alt="" />lendsql</h1>
+            <nav className="dashoard-container">
+                <div className="top_nav">
+                    <div className="logo">
+                                
+                        <h1 className="lsql"><img className="union" src={Union} alt="" />lendsql</h1>
+                    </div>
+                    <div className="input">
+                        <input typeof="text" placeholder="Search here...."/>
+                    </div>
+                    <div >
+                        <ul className="d-list">
+                            <li><a href="#">Docs</a></li>
+                            <li><img src={Notification} alt="" /></li>
+                            <li><img src={image4} alt="" /></li>
+                            <li><a href="#"></a></li>
+                            <li><img src={arrow_d} alt="" /></li>
+                        </ul>
+                    </div>
 
                 </div>
-                <div>
-                    <input typeof="text" placeholder="Search here...."/>
-                </div>
-                <div className="">
-                    <ul>
-                        <li><a href="#">Docs</a></li>
-                        <li><img src={Notification} alt="" /></li>
-                        <li><img src={image4} alt="" /></li>
-                        <li><a href="#"></a></li>
-                        <li><img src={arrow_d} alt="" /></li>
-                    </ul>
-                </div>
+                
+                
 
             </nav>
             <div className="container">
