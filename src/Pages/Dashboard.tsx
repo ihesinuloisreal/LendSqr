79067509abeacebcb9222a4906b53689d7e9c526
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-03 14:15:27 -0700

    dashboard

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -7,29 +7,63 @@ import arrow_d from "../images/arrow_d.png"
 export function Dashboard() {
     return (
         <>
-            <nav className="dashoard-container">
-                <div className="top_nav">
-                    <div className="logo">
-                                
-                        <h1 className="lsql"><img className="union" src={Union} alt="" />lendsql</h1>
-                    </div>
-                        <input typeof="text" placeholder="Search here...."/>
-                    <div >
-                        <ul className="d-list">
-                            <li><a href="#">Docs</a></li>
-                            <li><img src={Notification} alt="" /></li>
-                            <li><img src={image4} alt="" /></li>
-                            <li><a href="#"></a></li>
-                            <li><img src={arrow_d} alt="" /></li>
-                        </ul>
-                    </div>
-
+            
+            <div className="top_nav">
+                <div className="logo">
+                            
+                    <h1 className="lsql"><img className="union" src={Union} alt="" />lendsql</h1>
                 </div>
-                
-                
+                    <input typeof="text" placeholder="Search here...."/>
+                <div >
+                    <ul className="d-list">
+                        <li><a className="l1" href="#">Docs</a></li>
+                        <li><img className="l2" src={Notification} alt="" /></li>
+                        <li><img className="l3 " src={image4} alt="" /></li>
+                        <li><a href="#"></a></li>
+                        <li>Adedeji</li>
+                        <li className="l4"><img src={arrow_d} alt="" /></li>
+                    </ul>
+                </div>
 
-            </nav>
+            </div>
+            
             <div className="container">
+                <div className="side-bar">
+                    <nav>
+                        <ul>
+                            <li>Switch Organization</li>
+                            <li>Dashboard</li>
+                            <li>CUSTOMERS</li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div className="main">
+                    <div className="user-T"><a href="">User</a> </div>
+                    <div className="user-activity">
+                        <div className="u-1">
+                            <div>
+                                <img src="" alt="" />
+                            </div>
+                        </div>
+                        <div className="u-1">
+                            <div></div>
+                        </div>
+                        <div className="u-1">
+                            <div>
+                                
+                            </div>
+                        </div>
+                        <div className="u-1">
+                            
+                        </div>
+                    </div>
+                    <table>
+                        
+                    </table>
+                </div>
             </div>
         </>
     )
