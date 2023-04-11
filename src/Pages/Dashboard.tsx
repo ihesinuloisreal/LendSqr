8bcf1838e23ecf7186f625e8baeb72f55fb6bcdf
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-11 13:43:10 -0700

    table

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -3,13 +3,21 @@ import Union from "../images/Union.png"
 import Notification from "../images/Notification.png"
 import image4 from "../images/image 4.png"
 import arrow_d from "../images/arrow_d.png"
+import vector1 from "../images/Vector (1).png"
+import vector2 from "../images/Vector (2).png"
+import vector3 from "../images/Vector (3).png"
+import vector4 from "../images/Vector (4).png"
+import naira from "../images/₦.png"
+import tbr from '../images/tba.png'
+import btn from "../images/btn.png";
+
 
 export function Dashboard() {
     return (
         <>
             <div className="row">
                 <nav className="navbar navbar-expand-lg bg-body-tertiary center">
-                    <div className="container-fluid">
+                    <div className="container-fluid ">
                         
                             <div className="col-3">
                                 <a className="navbar-brand d-flex" href="#">
@@ -17,7 +25,7 @@ export function Dashboard() {
                                 
                                 </a>
                             </div>
-                            <div className="col-9 ">
+                            <div className="col-9">
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"></span>
                                 </button>
@@ -48,45 +56,29 @@ export function Dashboard() {
                     <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bgc side-n">
                         <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                             <a href="/" className="d-flex align-items-center  mb-md-0 me-md-auto text-white text-decoration-none">
-                                <span className="fs-5 d-none d-sm-inline ">Menu</span>
+                                <span className="fs-5  d-none d-sm-inline ">Menu</span>
                             </a>
                             <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start " id="menu">
                                 <li className="nav-item">
                                     <a href="#" className="nav-link align-middle px-0">
-                                        <i className="fs-4 bi-house"></i> <span className="ms-1 d-none d-sm-inline pb-5">Home</span>
+                                        <img src={vector1} alt="" /> <span className="ms-1 d-none d-sm-inline">Home</span><img src={arrow_d} alt="" />
                                     </a>
                                 </li>
                                 <li>
                                     <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                                        <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </a>
-                                    <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                                        <li className="w-100">
-                                            <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1 </a>
-                                        </li>
-                                        <li>
-                                            <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2 </a>
-                                        </li>
-                                    </ul>
+                                    <img src={vector2} alt="" /> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </a>
                                 </li>
                                 <li>
                                     <a href="#" className="nav-link px-0 align-middle">
-                                        <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Orders</span></a>
+                                    <img src={vector3} alt="" /> <span className="ms-1 d-none d-sm-inline">Orders</span></a>
                                 </li>
                                 <li>
                                     <a href="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
-                                        <i className="fs-4 bi-bootstrap"></i> <span className="ms-1 d-none d-sm-inline">Bootstrap</span></a>
-                                    <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                                        <li className="w-100">
-                                            <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1</a>
-                                        </li>
-                                        <li>
-                                            <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2</a>
-                                        </li>
-                                    </ul>
+                                    <img src={naira} alt="" /> <span className="ms-1 d-none d-sm-inline">Bootstrap</span></a>
                                 </li>
                                 <li>
                                     <a href="#submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                                        <i className="fs-4 bi-grid"></i> <span className="ms-1 d-none d-sm-inline">Products</span> </a>
+                                    <img src={vector4} alt="" /><span className="ms-1 d-none d-sm-inline">Products</span> </a>
                                         <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
                                         <li className="w-100">
                                             <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 1</a>
@@ -174,29 +166,69 @@ export function Dashboard() {
                             <table className="table bgc table-t">
                                 <thead>
                                     <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
+                                    <th scope="col text-upercase">organization <img src={tbr} alt="" /></th>
+                                    <th scope="col">Username <img src={tbr} alt="" /></th>
+                                    <th scope="col">Email <img src={tbr} alt="" /></th>
+                                    <th scope="col">Phone number <img src={tbr} alt="" /></th>
+                                    <th scope="col">Date joined <img src={tbr} alt="" /></th>
+                                    <th scope="col">Status <img src={tbr} alt="" /></th>
+                                    <th scope="col"></th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
+                                        <th scope="row">IENetwork</th>
+                                        <td >Larry the Bird</td>
+                                        <td >Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td><img src={btn} alt="" /></td>
                                     </tr>
                                     <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
+                                        <th scope="row">IENetwork</th>
+                                        <td >Larry the Bird</td>
+                                        <td >Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td><img src={btn} alt="" /></td>
                                     </tr>
                                     <tr>
-                                    <th scope="row">3</th>
-                                    <td colSpan={2}>Larry the Bird</td>
-                                    <td>@twitter</td>
+                                        <th scope="row">IENetwork</th>
+                                        <td >Larry the Bird</td>
+                                        <td >Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td><img src={btn} alt="" /></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">IENetwork</th>
+                                        <td >Larry the Bird</td>
+                                        <td >Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td><img src={btn} alt="" /></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">IENetwork</th>
+                                        <td >Larry the Bird</td>
+                                        <td >Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td><img src={btn} alt="" /></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">IENetwork</th>
+                                        <td >Larry the Bird</td>
+                                        <td >Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td><img src={btn} alt="" /></td>
                                     </tr>
                                 </tbody>
                             </table>
